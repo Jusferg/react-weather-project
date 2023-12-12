fcbc60564a90f6a3bc368247c919fdc69e49240d
Tia <90842451+Jusferg@users.noreply.github.com>
2023-12-12 09:25:19 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <h1>
-          Hello World!!!!
-        </h1>
-       
       </header>
     </div>
   );
